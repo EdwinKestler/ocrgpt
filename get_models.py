e43642b6ae17787f49c98ef2295558f810981458
Edwin Kestler <kestler@flatbox.co>
2023-04-04 23:56:07 -0600

    changed display models

--- get_models.py
+++ get_models.py
@@ -2,5 +2,8 @@ import os
 import openai
 openai.organization = "org-P44J2sYyfEEfBF2gCYM7wSgX"
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
 models = openai.Model.list()
-print(models)
+
+for model in models['data']:
+    print(f"{model.id}: {model.owned_by}")
